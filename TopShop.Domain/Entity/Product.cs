e10d177c8387da81d4799a15b2d251b5c6a6d932
This PC <This PC@DESKTOP-FF6PJP8>
2026-04-20 21:22:36 +0700

    feat(Domain): Thêm phương thức EnsureActive Cho Product

--- TopShop.Domain/Entity/Product.cs
+++ TopShop.Domain/Entity/Product.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TopShop.Domain.Enum;
 using TopShop.Domain.ValueObject;
 
 namespace TopShop.Domain.Entity
@@ -14,6 +15,7 @@ namespace TopShop.Domain.Entity
         public ProductDescription Descriptions { get; private set; } 
         public Price Price { get; private set; }
         public Quantity Quantity { get; private set; }
+        public ProductStatus Status { get; private set; } = ProductStatus.Activate; // Trạng thái hoạt động của sản phẩm (mặc định là đang bán)
         private Product() { }
         public Product(ProductName productName, ProductDescription descriptions, Price price, Quantity quantity)
         {
@@ -22,31 +24,55 @@ namespace TopShop.Domain.Entity
             Price = price;
             Quantity = quantity;
         }
+
+        private void EnsureActive()
+        {
+            if (Status != ProductStatus.Activate)
+                throw new InvalidOperationException("Sản phẩm không hoạt động.");
+        }
+
         // các hành vi thay đổi trạng thái của sản phẩm
         public void ChangeName(ProductName newName)
         {
+            EnsureActive();
             Name = newName;
         }
         public void ChangeDescriptions(ProductDescription newDescriptions)
         {
+            EnsureActive();
             Descriptions = newDescriptions;
         }
+        
         public void ChangeQuantity(Quantity newQuantity)
         {
+            EnsureActive();
             Quantity = newQuantity;
         }
 
         public void ChangePrice(Price newPrice)
         {
+            EnsureActive();
             Price = newPrice;
         }
         // Phương thức để giảm số lượng sản phẩm khi có đơn hàng
-        public void ReduceStock(int amount)
+        public void ReduceStock(Quantity amount)
         {
-            if (amount > Quantity.Value)
+            EnsureActive();
+            if (amount.Value > Quantity.Value)
                 throw new InvalidOperationException("Không đủ hàng trong kho.");
-            Quantity = new Quantity(Quantity.Value - amount);
+            Quantity = new Quantity(Quantity.Value - amount.Value);
         }
+        // Phương thức để mở bán 1 sản phẩm 
+        public void Activate()
+        {
+            Status = ProductStatus.Activate;
+        }
+        // Phương thức để ngừng bán 1 sản phẩm
+        public void Deactivate()
+        {
+            Status = ProductStatus.Inactive;
+        }
+
         //Phương thức tính tổng giá trị của sản phẩm dựa trên số lượng và giá
         public decimal CalculateTotalValue()
         {
---
+++ TopShop.Domain/Enum/ProductStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopShop.Domain.Enum
+{
+    public enum ProductStatus
+    {
+        // các trạng thái của sản phẩm
+        Activate = 1,
+        Inactive = 2,
+      
+    }
+}
